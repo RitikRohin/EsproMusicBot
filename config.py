9e318e9ab714a597ed89753a574eac3c8b788c17
RitikRohin <irthaiwan@gmail.com>
2025-10-10 22:09:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ OWNER_ID = int(getenv("OWNER_ID", None))
 
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+YT_API_KEY = getenv("YT_API_KEY" , "xbit_3ENGZIE7WXYVAB17C3MHX1" ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
