e9544ba4a40fd6425b00041422cd62a52e0ba5ee
RitikRohin <irthaiwan@gmail.com>
2025-11-12 16:32:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,9 +32,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ritikrohin/EsproMusicBot",
+    "https://github.com/RitikRohin/EsproMusicBot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ritik")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
